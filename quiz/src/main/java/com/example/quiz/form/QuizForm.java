29e3507156ce85491b8417c2c06d68b91b78c3a1
qhdudedi <86972598+qhdudedi@users.noreply.github.com>
2023-03-07 00:09:29 +0900

    Create QuizForm.java

---
+++ quiz/src/main/java/com/example/quiz/form/QuizForm.java
@@ -0,0 +1,26 @@
+package com.example.quiz.form;
+
+import javax.validation.constraints.NotBlank;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class QuizForm {
+    private Integer id;
+
+    //퀴즈 내용
+    @NotBlank
+    private String question;
+   //퀴즈 해답
+    private Boolean answer;
+    //작성자
+    @NotBlank
+    private String author;
+    /**등록 또는 변경 판단용 */
+    private Boolean newQuiz;
+}
