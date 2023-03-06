d34385c34a9d2a9888441853d0b99e0d82ed7a76
qhdudedi <86972598+qhdudedi@users.noreply.github.com>
2023-03-06 23:22:44 +0900

    Create QuizApplication.java

---
+++ quiz/src/main/java/com/example/quiz/QuizApplication.java
@@ -0,0 +1,13 @@
+package com.example.quiz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import javax.swing.*;
+
+@SpringBootApplication
+public class QuizApplication{
+	public static void main(String[] args){
+		SpringApplication.run(QuizApplication.class, args);
+	}
+}
