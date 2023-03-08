46dd8b89c9d2005b23f40c49019f617484e7088d
qhdudedi <86972598+qhdudedi@users.noreply.github.com>
2023-03-09 00:09:09 +0900

    Update QuizForm.java

--- quiz/src/main/java/com/example/quiz/form/QuizForm.java
+++ quiz/src/main/java/com/example/quiz/form/QuizForm.java
@@ -1,7 +1,4 @@
 package com.example.quiz.form;
-
-import javax.validation.constraints.NotBlank;
-
 import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Data;
